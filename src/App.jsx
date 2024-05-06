bb509d10c6d969ce861a1918590e2e2306f5f984
Michael Wood <michaelwood33311@icloud.com>
2024-05-06 10:30:38 -0400

    added Home component shell

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,28 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import { useDispatch, useSelector } from 'react-redux';
 import { counterDecrement, counterIncrement, counterSetValue } from './store/sliceCounter';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 
 function App() {
   const dispatch = useDispatch();
   const count = useSelector(state => state.counter)
   return (
-    <>
-      <div className='text-blue-600 cursor-pointer' onClick={() => dispatch(counterIncrement())}>+</div>
-      <div>{count}</div>
-      <div className='text-blue-600 cursor-pointer' onClick={() => dispatch(counterDecrement())}>-</div>
-      <input type="number" onChange={e => dispatch(counterSetValue(e.target.value))} />
-    </>
+    <div className="App">
+   <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/post/:id" element={<Post />} />
+        <Route path="/purchase" element={<Purchase />}/>
+        <Route path="/terms-and-conditions" element={<Terms />}/>
+        <Route path="/privacy-policy" element={<Privacy />}/>
+        <Route path="/password-reset" element={<PasswordReset />}/>
+        <Route path="/packages" element={<Packages />}/>
+        <Route path="/edit" element={<JoditWYSIWYG />}/>
+      </Routes>
+    </Router>
+  </div>
   )
 }
 
---
+++ src/components/Home.jsx
@@ -0,0 +1,10 @@
+import './Home.scss';
+import React from 'react'
+
+function Home() {
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home
