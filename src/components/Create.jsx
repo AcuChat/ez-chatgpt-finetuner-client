38116c24f988258d38d0fcc55c39ae5b275bb152
Michael Wood <michaelwood33311@icloud.com>
2024-05-06 14:10:35 -0400

    added getting all needed information in Create component

--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -1,11 +1,44 @@
 import './Create.scss';
-import React from 'react'
+import React, {useCallback, useEffect, useState} from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { projectsSetNewProject } from '../store/sliceProjects';
+import Dropzone, {useDropzone} from 'react-dropzone'
 
 function Create() {
+  const [ file, setFile ] = useState(null);
+
+  const models = [
+    'gpt-4-turbo',
+    'gpt-3.5-turbo',
+    'gpt-4',
+    'gpt-4-32k'
+  ]
+
   const dispatch = useDispatch();
   const projectInfo = useSelector(state => state.projects);
+
+  const droppedFiles = acceptedFiles => {
+    if (!acceptedFiles.length) return;
+    const name = acceptedFiles[0]?.name;
+    const loc = name.lastIndexOf('.');
+    const extension = name.substring(loc+1);
+    if (extension !== 'csv' && extension !== 'jsonl') return alert ('Error: File must be CSV or JSONL.');
+    setFile(acceptedFiles[0])
+  }
+
+  const handleSubmit = () => {
+    if (!file) return alert("Error: missing file");
+    if (!projectInfo?.newProject?.name) return alert ("Error: missing project name");
+    const systemPrompt = projectInfo?.newProject?.systemPrompt ? projectInfo.newProject.systemPrompt : '';
+    const userPrompt = projectInfo?.newProject?.userPrompt ? projectInfo.newProject.userPrompt : '';
+    if (!userPrompt && !systemPrompt) return alert ("Error: must provide system prompt or user prompt");
+
+
+  }
+
+  useEffect(() => {
+    if (!projectInfo?.newProject?.model) dispatch(projectsSetNewProject({model: models[0]}))
+  })
 
   return (
     <div className='Create'>
@@ -22,10 +55,30 @@ function Create() {
         value={projectInfo?.newProject?.userPrompt ? projectInfo.newProject.userPrompt : ''} 
         onChange={(e) => dispatch(projectsSetNewProject({userPrompt: e.target.value}))}
       />
-      <div className="Create__dropzone">
-        Drop Zone
-      </div>
-      <div className="button">Submit</div>
+      {!file && <div className="Create__dropzone">
+          <Dropzone onDrop={acceptedFiles => droppedFiles(acceptedFiles)}>
+            {({getRootProps, getInputProps}) => (
+              <section>
+                <div {...getRootProps()}>
+                  <input {...getInputProps()} />
+                  <p>Drag 'n' drop some files here, or click to select files</p>
+                </div>
+              </section>
+            )}
+          </Dropzone>
+        </div>
+      }
+      {
+        file && <div className="Create__filename"><b>{file?.name}</b></div>
+      }
+      <select value={projectInfo?.newProject?.model ? projectInfo.newProject.model : models[0]} onChange={(e) => dispatch(projectsSetNewProject({model: e.target.value}))}>
+        {models.map(model => {
+          return (
+            <option key={model} value={model}>{model}</option>
+          )
+        })}
+      </select>
+      <div className="button" onClick={handleSubmit}>Submit</div>
       <div className="button-outline">Clear</div>
     </div>
   )
