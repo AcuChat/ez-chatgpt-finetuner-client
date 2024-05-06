1ba4053a556eace1dcb35b542ebc307c4247d7d3
Michael Wood <michaelwood33311@icloud.com>
2024-05-06 10:33:54 -0400

    added Edit component shell

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import { counterDecrement, counterIncrement, counterSetValue } from './store/sli
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Home from './components/Home';
 import Create from './components/Create';
+import Edit from './components/Edit';
 
 function App() {
   const dispatch = useDispatch();
@@ -18,6 +19,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/create" element={<Create />} />
+        <Route path="/edit" element={<Edit />} />
+        
       </Routes>
     </Router>
   </div>
---
+++ src/components/Edit.jsx
@@ -0,0 +1,10 @@
+import './Edit.scss';
+import React from 'react'
+
+function Edit() {
+  return (
+    <div>Edit</div>
+  )
+}
+
+export default Edit
