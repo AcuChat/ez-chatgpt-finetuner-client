95fc3d408dec84db581371db17b35878e1d4c4cf
Michael Wood <michaelwood33311@icloud.com>
2024-05-06 10:40:15 -0400

    added AugmentData component shell

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import Home from './components/Home';
 import Create from './components/Create';
 import Edit from './components/Edit';
 import FineTune from './components/FineTune';
+import AugmentData from './components/AugmentData';
 
 function App() {
   const dispatch = useDispatch();
@@ -22,6 +23,7 @@ function App() {
         <Route path="/create" element={<Create />} />
         <Route path="/edit" element={<Edit />} />
         <Route path="/finetune" element={<FineTune />} />
+        <Route path="/augment-data" element={<AugmentData />} />
       </Routes>
     </Router>
   </div>
---
+++ src/components/AugmentData.jsx
@@ -0,0 +1,10 @@
+import './AugmentData';
+import React from 'react'
+
+function AugmentData() {
+  return (
+    <div>AugmentData</div>
+  )
+}
+
+export default AugmentData
