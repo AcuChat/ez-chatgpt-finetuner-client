ee6c7c2034a524c6dc163bae611624abd26b9869
Michael Wood <michaelwood33311@icloud.com>
2024-05-06 12:53:22 -0400

    added create button for Home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,22 @@
 import './Home.scss';
 import React from 'react'
+import { IoMdAddCircleOutline } from "react-icons/io";
+import { Link } from 'react-router-dom';
 
 function Home() {
+  const addProject = () => {
+    alert ('add Project')
+  }
   return (
-    <div>Home</div>
+    <div className='Home'>
+      <h1 className="Home__title">Home</h1>
+      <div className="Home__add-project">
+        <Link to="/create">
+          <IoMdAddCircleOutline className='Home__add-button' size={32} color="black"/>
+        </Link>
+        <div className="Home__right-label">Add Project</div>
+      </div>
+    </div>
   )
 }
 
