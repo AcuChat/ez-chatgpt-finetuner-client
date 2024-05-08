9f391d55161166be50969dae640711d9ed723ec6
Michael Wood <michaelwood33311@icloud.com>
2024-05-08 12:54:13 -0400

    added top part of edit component

--- src/components/Edit.jsx
+++ src/components/Edit.jsx
@@ -1,9 +1,24 @@
 import './Edit.scss';
 import React from 'react'
+import { useParams, Navigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
 
 function Edit() {
+  const { id } = useParams();
+  const projects = useSelector(state => state?.projects?.projects);
+  if (!projects.length) {
+    return <Navigate to="/"/>
+  }
+
+  console.log('projects', projects)
+  const project = projects.find(p => p.project_id === id);
+  console.log('project', project)
   return (
-    <div>Edit</div>
+    <div className='Edit'>
+      <h1>Edit</h1>
+      <h2 className='Edit__project-name'>{project.project_name}</h2>
+      <div className="Edit__submit-button">Submit</div>
+    </div>
   )
 }
 
