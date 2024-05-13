f6887e66751210da77e0b51855cce9cf801b71c8
Michael Wood <michaelwood33311@icloud.com>
2024-05-13 11:02:23 -0400

    added displaying project name in Status component

--- src/components/Status.jsx
+++ src/components/Status.jsx
@@ -1,9 +1,20 @@
 import './Status.scss';
 import React from 'react'
+import { useSelector } from 'react-redux';
+import { useParams, Navigate } from 'react-router-dom';
 
 function Status() {
+  const { id } = useParams();
+
+  const projects = useSelector(state => state?.projects?.projects);
+  const server = useSelector(state => state.projects?.server);
+
+  const project = projects?.find(p => p.project_id === id);
   return (
-    <div>Status</div>
+    <div className='Status'>
+      <h1>Status</h1>
+      <h2 className='Edit__project-name'>{project?.project_name}</h2>
+    </div>
   )
 }
 
